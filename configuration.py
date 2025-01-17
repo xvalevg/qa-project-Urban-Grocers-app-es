50df5b4da020c6802d804a8fe5c83f724295b1b3
desarrolladoSenior2Saggaac <144712277+desarrolladoSenior2Saggaac@users.noreply.github.com>
2025-01-16 23:23:37 -0500

    Change url_service

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://cnt-1ab47bd0-97d9-43c6-a830-b40b8caed448.containerhub.tripleten-services.com"
-DOC_PATH = "https://cnt-1ab47bd0-97d9-43c6-a830-b40b8caed448.containerhub.tripleten-services.com/docs/"
+URL_SERVICE = "https://cnt-cdd6461a-781f-4103-8b93-201889d7d471.containerhub.tripleten-services.com"
+DOC_PATH = "https://cnt-cdd6461a-781f-4103-8b93-201889d7d471.containerhub.tripleten-services.com/docs/"
 CREATE_USER_PATH = "/api/v1/users"
 KITS_PATH = "/api/v1/kits"
